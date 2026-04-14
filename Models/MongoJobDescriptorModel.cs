d51558c5482494b588cc86443daea57a868eb6c1
František Bereň <birko@live.com>
2026-04-14 16:33:57 +0200

    fix: Add missing using directive for Birko.BackgroundJobs.Models in MongoJobDescriptorModel

--- Models/MongoJobDescriptorModel.cs
+++ Models/MongoJobDescriptorModel.cs
@@ -1,6 +1,7 @@
 using System;
 using Birko.Data.Models;
 using Birko.BackgroundJobs.Serialization;
+using Birko.BackgroundJobs.Models;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Birko.BackgroundJobs.MongoDB.Models;
